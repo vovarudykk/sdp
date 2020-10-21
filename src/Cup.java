29f70c3f4bf7bb790016a7f64f63cf4a33c2281c
Ted Mosby <41184216+vovarudykk@users.noreply.github.com>
2020-10-21 15:08:10 +0300

    Update Cup.java

--- src/Cup.java
+++ src/Cup.java
@@ -1,16 +1,11 @@
 public class Cup extends Dishes{
-    private boolean handle;
+    private Handle handle;
 
-    public Cup(String name, Material material, String producer, boolean handle, Size sizeOfCup) {
+    public Cup(String name, Material material, String producer, Handle handle, Size sizeOfCup) {
         super(name, material, producer, sizeOfCup);
         this.handle = handle;
     }
-    public Cup(String name, Material material, boolean handle, Size sizeOfCup) {
-        super(name, material, "Unknown", sizeOfCup);
-        this.handle = handle;
+    public Cup(String name, Material material, String producer, Size sizeOfCup) {
+        super(name, material, producer, sizeOfCup);
     }
-
-    public boolean isHandle() {return handle;}
-
-    public void setHandle(boolean handle) {this.handle = handle;}
 }
