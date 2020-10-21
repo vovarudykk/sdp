bc36d6e287e802e02544a7a7b4a37d47df6d792c
Ted Mosby <41184216+vovarudykk@users.noreply.github.com>
2020-10-21 15:10:40 +0300

    Update Cup.java

--- src/Cup.java
+++ src/Cup.java
@@ -1,9 +1,7 @@
 public class Cup extends Dishes{
-    private Handle handle;
 
     public Cup(String name, Material material, String producer, Handle handle, Size sizeOfCup) {
         super(name, material, producer, sizeOfCup);
-        this.handle = handle;
     }
     public Cup(String name, Material material, String producer, Size sizeOfCup) {
         super(name, material, producer, sizeOfCup);
