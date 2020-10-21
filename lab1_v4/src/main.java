cbc4dd0e90e611a709c01167179659e9b8c28882
Ted Mosby <41184216+vovarudykk@users.noreply.github.com>
2020-10-21 12:19:39 +0300

    Update main.java
    add call func  getStringAll and rename object List

--- lab1_v4/src/main.java
+++ lab1_v4/src/main.java
@@ -3,24 +3,22 @@ import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) throws InterruptedException {
-        ArrayList<Dinnerware> ListService1 = new ArrayList<>();
-        ArrayList<Cutlery> ListService2 = new ArrayList<>();
+        ArrayList<Dinnerware> ListDinnerware = new ArrayList<>();
+        ArrayList<Cutlery> ListCutlery = new ArrayList<>();
 
         Dinnerware plate3 = new Plate("Plate 3", Color.GREY, "Russia");
         Dinnerware cup2 = new Cup("Cup 2", Color.WHITE, "Italy", 0.25);
-        ListService1.add(plate3);
-        ListService1.add(cup2);
+        ListDinnerware.add(plate3);
+        ListDinnerware.add(cup2);
 
         Cutlery fork1 = new Fork("Fork 1", Material.STAINLESS_STEEL, 30, "Ukraine");
         Cutlery spoon1 = new Spoon("Spoon 1", Material.STEEL, 25, "Russia");
-        ListService2.add(fork1);
-        ListService2.add(spoon1);
+        ListCutlery.add(fork1);
+        ListCutlery.add(spoon1);
 
-        Service service1 = new Service(ListService1, ListService2);
+        Service service1 = new Service(ListDinnerware, ListCutlery);
         System.out.println(service1.toString());
 
         System.out.println(Material.getStringAll());
-
-
     }
 }
