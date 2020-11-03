18040afb50e72211240e4efc9c172d23c140ed52
Ted Mosby <41184216+vovarudykk@users.noreply.github.com>
2020-11-03 21:18:38 +0200

    Update Cup.java
    Add equals and assotiation

--- src/Cup.java
+++ src/Cup.java
@@ -1,9 +1,29 @@
 public class Cup extends Dishes{
 
+    private final boolean hasHandle;
+    private Material materialHandle;
+    
     public Cup(String name, Material material, String producer, Handle handle, Size sizeOfCup) {
         super(name, material, producer, sizeOfCup);
+        this.hasHandle = checkHandle(handle);
     }
     public Cup(String name, Material material, String producer, Size sizeOfCup) {
         super(name, material, producer, sizeOfCup);
+        this.hasHandle = false;
+    }
+
+    public boolean checkHandle(Handle handle) {
+        this.materialHandle = handle.getMaterial();
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var objTemp = (Cup) obj;
+        return objTemp.getName().equals(this.getName()) && objTemp.getMaterial() == this.getMaterial() &&
+                objTemp.getProducer().equals(this.getProducer()) && objTemp.getSize() == this.getSize();
     }
 }
