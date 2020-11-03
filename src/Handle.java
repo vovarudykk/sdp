296cb6737653b14a96606b9a93c2f11432092ddb
Ted Mosby <41184216+vovarudykk@users.noreply.github.com>
2020-11-03 21:19:31 +0200

    Update Handle.java
    Add interface implements

--- src/Handle.java
+++ src/Handle.java
@@ -1,7 +1,21 @@
-public class Handle {
+public class Handle implements Printable{
     private Material material;
 
     public Handle(Material material) {
         this.material = material;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+
+    @Override
+    public void printData() {
+        System.out.println("Handle material is " + material);
     }
 }
