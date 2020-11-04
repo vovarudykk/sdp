d6370742e4529053200451f5f4d5f57b233b8488
Ted Mosby <41184216+vovarudykk@users.noreply.github.com>
2020-11-04 09:45:02 +0200

    Update Plate.java

--- src/Plate.java
+++ src/Plate.java
@@ -3,4 +3,7 @@ public class Plate extends Dishes{
     public Plate(String name, Material material, String producer, Size sizeOfPlate) {
         super(name, material, producer, sizeOfPlate);
     }
+    public Plate(int name, Material material, String producer, Size sizeOfPlate) {
+        super(name, material, producer, sizeOfPlate);
+    }
 }
