667f1e5f455080f708b39e3675baac337fe022d8
Ted Mosby <41184216+vovarudykk@users.noreply.github.com>
2020-11-08 15:27:12 +0200

    Update SaltShaker.java
    add few method and improve class Salt

--- src/SaltShaker.java
+++ src/SaltShaker.java
@@ -1,17 +1,34 @@
 public class SaltShaker extends Dishes{
-    private static double salt;
+    private final Salt salt = new Salt();
 
     public SaltShaker(String name, Material material, String producer, Size sizeOfShaker) {
         super(name, material, producer, sizeOfShaker);
     }
 
+    public void addSaltInSH(double count) {
+        salt.addSalt(count);
+    }
+
+    public double getSaltCount() {
+        return this.salt.getCount();
+    }
 
     private static class Salt {
-        public void addSalt(double salt) {
-            SaltShaker.salt += salt;
+        private double count;
+
+        public Salt() {
+            this.count = 0;
         }
-        public void addSalt(int salt) {
-            SaltShaker.salt += salt;
+
+        public void addSalt(double add) {
+            this.count += add;
+        }
+        public void addSalt(int add) {
+            this.count += add;
+        }
+
+        public double getCount() {
+            return this.count;
         }
     }
 }
